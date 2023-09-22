cf2e77ec932c917c041babeaf32f0a23209757ce
lochanamalt <lochanatharanganie@gmail.com>
2023-09-22 14:16:48 +0530

    set home to profile

--- src/App.js
+++ src/App.js
@@ -153,7 +153,7 @@ export default function App() {
         {layout === "vr" && <Configurator />}
         <Switch>
           {getRoutes(routes)}
-          <Redirect from="*" to="/dashboard" />
+          <Redirect from="*" to="/profile" />
         </Switch>
       </ThemeProvider>
     </CacheProvider>
@@ -177,7 +177,7 @@ export default function App() {
       {layout === "vr" && <Configurator />}
       <Switch>
         {getRoutes(routes)}
-        <Redirect from="*" to="/dashboard" />
+        <Redirect from="*" to="/profile" />
       </Switch>
     </ThemeProvider>
   );
